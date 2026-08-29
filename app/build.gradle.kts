a3a77b2d567f784bf499f1286fa132c73a60baec
Notix <Enlpot@users.noreply.github.com>
2026-08-29 17:40:11 +0800

    chore: bump version to 8.20.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.enlpot.notix"
         minSdk = 24
         targetSdk = 36
-        versionCode = 134
-        versionName = "8.19.0"
+        versionCode = 135
+        versionName = "8.20.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
