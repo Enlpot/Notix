4738b8ef0697ac589c89e1a42cf8c909d2ef7f16
Notix <Enlpot@users.noreply.github.com>
2026-08-30 17:18:54 +0800

    v8.34: 升版

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.enlpot.notix"
         minSdk = 24
         targetSdk = 36
-        versionCode = 148
-        versionName = "8.33.0"
+        versionCode = 149
+        versionName = "8.34.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
