cf727b5deeb5517e8ee6f5901b09600c622a9fa9
Notix <Enlpot@users.noreply.github.com>
2026-08-31 14:30:00 +0800

    升版 8.43.0
    - 通知热词词云功能发布

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.enlpot.notix"
         minSdk = 24
         targetSdk = 36
-        versionCode = 163
-        versionName = "8.42.2"
+        versionCode = 164
+        versionName = "8.43.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -100,6 +100,7 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
+
 
 
 
